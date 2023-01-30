ea5d72bc7f197c5ee8eedbea7f7f8c201f6fc2dc
VinGeo1 <123502241+VinGeo1@users.noreply.github.com>
2023-01-30 07:46:59 -0500

    Update simpleStorage.js

--- test/simpleStorage.js
+++ test/simpleStorage.js
@@ -1,23 +1,23 @@
 const SimpleStorage = artifacts.require("SimpleStorage") 
 
 contract('SimpleStorage', (accounts) => {
+	        //Two test case
 		const [Vineeth, Amanda] = accounts
+		const amount = 0;
 		it("Verify Vineeth and Amanda's favorite numbers default to 0", async () => {
 					const ssContract = await SimpleStorage.deployed()
 					const VineethNum = await ssContract.favoriteNumbers.call(Vineeth)
-					assert.equal(VineethNum, 0,
-									"Vineeth's default value was non-zero")
+					assert.equal(VineethNum, amount,"Vineeth's default value was non-zero")
 					
 					const AmandaNum = await ssContract.favoriteNumbers.call(Amanda)
-					assert.equal(AmandaNum, 0,
-									"Amanda's default value was non-zero")
+					assert.equal(AmandaNum, amount,"Amanda's default value was non-zero")
 				})
-
+                const amount1 = 99;
+	        const amount2 = 45;
 		it("Set Vineeth's favorite number to 99", async () => {
 					const ssContract = await SimpleStorage.deployed()
-					ssContract.setFavorite(99, {from: Vineeth})
+					ssContract.setFavorite(amount1, {from: Vineeth})
 					const newVineethNum = await ssContract.favoriteNumbers.call(Vineeth)
-					assert.equal(newVineethNum, 45,
-									"Vineeth's new value was not 99")
+					assert.equal(newVineethNum,amount2,Vineeth's new value was not 99")
 				})
 })
